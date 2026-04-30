e9fdd6bd00ba1f5e1e1b366ccd40c4d31d499a20
Redware-Dev <redwareinfo@gmail.com>
2026-04-30 09:47:09 -0300

    Correção IP
    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,6 +67,7 @@ export default function Splash() {
   // 🔹 URL base da API
   function getApiBaseUrl(ip: string) {
     if (ip.startsWith("177.54.239.199")) {
+      //return "http://82.25.79.218:4143/api/SqlApp";
       return "http://10.1.1.135:4143/api/SqlApp";
     }
     return "http://177.54.239.199:4143/api/SqlApp";
