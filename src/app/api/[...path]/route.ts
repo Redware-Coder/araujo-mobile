fa1d976d114b8a3db70b005a1fe3944cd79b5e56
Redware-Dev <redwareinfo@gmail.com>
2026-05-06 13:55:40 -0300

    c9
    9

--- src/app/api/[...path]/route.ts
+++ src/app/api/[...path]/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+
+// 🔵 GET (já funcionando)
 export async function GET(
   req: NextRequest,
   context: { params: Promise<{ path: string[] }> }
@@ -18,6 +20,36 @@ export async function GET(
   } catch (err) {
     return NextResponse.json(
       { error: "Erro ao conectar API externa" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  context: { params: Promise<{ path: string[] }> }
+) {
+  const { path } = await context.params;
+
+  const body = await req.text(); // pega o body original
+
+  const url = `http://177.54.239.199:4143/api/SqlApp/${path.join("/")}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body,
+    });
+
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Erro ao enviar dados para API externa" },
       { status: 500 }
     );
   }
